5c84b740923b38aff76547b6f3af2f4001b7d10e
Heather Piwowar <hpiwowar@gmail.com>
2010-12-02 11:06:24 -0800

    add plots to view distributions

--- scripts/descriptive_stats.R
+++ scripts/descriptive_stats.R
@@ -2,18 +2,36 @@
 # Placeholder content.  Will make more browseable soon.
 
 metadataColumns = c("doi", "pubDate", "pubDateValue", "daysSincePublished", "journal")
-altmetricsColumns = names(dat.all)[names(dat.all) %nin% metadataColumns]
+altmetricsColumns = names(dat.eventcounts)[names(dat.eventcounts) %nin% metadataColumns]
 
-dat.all.metricIsUsed = dat.all
+dat.metricIsUsed = dat.eventcounts
 
-dat.all.metricIsUsed[,altmetricsColumns][dat.all.metricIsUsed[,altmetricsColumns] > 1] = 1
-summary(dat.all.metricIsUsed)
+dat.metricIsUsed[,altmetricsColumns][dat.metricIsUsed[,altmetricsColumns] > 1] = 1
+summary(dat.metricIsUsed)
 
 options(scipen=100)
 options(digits=2)
 
 # Metrics by Average number of articles with at least one event
-apply(dat.all.metricIsUsed[,altmetricsColumns], 2, mean)
+apply(dat.metricIsUsed[,altmetricsColumns], 2, mean)
 
 # Number of articles by how many metrics they have with at least one event
-table(apply(dat.all.metricIsUsed[,altmetricsColumns], 1, sum))
+table(apply(dat.metricIsUsed[,altmetricsColumns], 1, sum))
+
+# Look at the distributions
+for (col in altmetricsColumns) {
+	quartz()
+	par(mfrow = c(2, 1))
+	hist(dat.eventcounts[,col], main=col)
+	hist(log(dat.eventcounts[,col]), main=paste("log(", col, ")", sep=""))
+}
+
+### Write out to examine in other programs
+write.csv(dat.metricIsUsed, "data/derived/all_metrics_used.csv")
+
+library(Hmisc)
+Hmisc::describe(dat.eventcounts[,altmetricsColumns])
+latex(Hmisc::describe(dat.eventcounts[,altmetricsColumns]))
+
+Hmisc::describe(dat.metricIsUsed[,altmetricsColumns])
+
