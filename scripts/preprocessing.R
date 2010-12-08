40abe6615b1c8eb5683195628e13b96c8fac951c
Heather Piwowar <hpiwowar@gmail.com>
2010-12-08 08:43:34 -0600

    revisions to handle NAs in raw data

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -10,10 +10,29 @@ dat.raw.eventcounts = read.csv("data/raw/event_counts.txt", header=TRUE, sep="\t
 ## Look at it
 dim(dat.raw.eventcounts)
 names(dat.raw.eventcounts)
-summary(dat.eventcounts)
+summary(dat.raw.eventcounts)
+
+## A bit of data cleaning
+dat.eventcounts = dat.raw.eventcounts
+
+# get rid of rows that have dates that are NA
+library(date)
+
+# Create a column that contains the date as a date object
+years = as.integer(substr(dat.raw.eventcounts$pubDate, 1, 4))
+months = as.integer(substr(dat.raw.eventcounts$pubDate, 6, 7))
+days = as.integer(substr(dat.raw.eventcounts$pubDate, 9, 10))
+has.good.date = years > 0
+
+# Now create a date type variable
+dat.eventcounts$pubDateValue[has.good.date]  = mdy.date(months[has.good.date], days[has.good.date], years[has.good.date])
+dat.eventcounts$pubDateValue[years <= 0]  = NA
+
+# Create a column that has days since published
+dat.eventcounts$daysSincePublished = max(dat.eventcounts$pubDateValue, na.rm=T) - dat.eventcounts$pubDateValue
+
 
 ## Adjust some fields to they are the right datatype.  
-dat.eventcounts = dat.raw.eventcounts
 
 # Change journal strings to factors
 dat.eventcounts$journal = factor(dat.raw.eventcounts$journal)
@@ -22,35 +41,19 @@ dat.eventcounts$journal = factor(dat.raw.eventcounts$journal)
 dat.eventcounts$f1000Factor = as.integer(dat.raw.eventcounts$f1000Factor)
 dat.eventcounts$f1000Factor[is.na(dat.eventcounts$f1000Factor)] = 0
 
-library(date)
-# Create a column that contains the date as a date object
-days = as.integer(substr(dat.raw.eventcounts$pubDate, 9, 10))
-months = as.integer(substr(dat.raw.eventcounts$pubDate, 6, 7))
-years = as.integer(substr(dat.raw.eventcounts$pubDate, 1, 4))
-dat.eventcounts$pubDateValue  = mdy.date(months, days, years)
+# Change deliciousCount strings to integer counts.  "false" means count of 0.
+dat.eventcounts$deliciousCount = as.integer(dat.raw.eventcounts$deliciousCount)
 
-# Create a column that has days since published
-dat.eventcounts$daysSincePublished = max(dat.eventcounts$pubDateValue) - dat.eventcounts$pubDateValue
 
-# There are a few rows that don't have PLoS XML data.  Set missing values to NA
-plosMissing = dat.raw.eventcounts$htmlDownloadsCount == 0
-plosColumns = c("almBlogsCount",
-	"pdfDownloadsCount",
-	"xmlDownloadsCount",
-	"htmlDownloadsCount",
-	"almCiteULikeCount",
-	"almScopusCount",
-	"almPubMedCentralCount",
-	"almCrossRefCount",
-	"plosCommentCount",
-	"plosCommentResponsesCount")
-dat.eventcounts[plosMissing, plosColumns] = NA
+# rename PMC column
+dat.eventcounts$almPubMedCentralCount = dat.eventcounts$almPubMedCount
+dat.eventcounts$almPubMedCount = NULL
 
 # There are a few Facebook results from Facebook API with negative numbers
 # Not clear what this means (not in Facebook API docs), so setting to NA
 facebookColumns = c("facebookShareCount", "facebookLikeCount", "facebookCommentCount", "facebookClickCount")
 for (col in facebookColumns) {
-	dat.eventcounts[dat.eventcounts[, col] < 0, col] = NA	
+	dat.eventcounts[which(dat.eventcounts[, col] < 0), col] = NA	
 }
 
 
